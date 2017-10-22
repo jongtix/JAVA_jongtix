a45c2520a58529975e487c4d664caba4808962b3
jongtix <jong1145@naver.com>
2017-10-22 12:52:55 +0900

    home 171022

--- src/Example1sol.java
+++ src/Example1sol.java
@@ -27,7 +27,7 @@ public class Example1sol {
 		}
 		System.out.println();
 		
-		// 3
+		// 3 a
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < i * 2 + 1; j++) {
 				if (j%2 == 0) {
@@ -40,9 +40,49 @@ public class Example1sol {
 		}
 		System.out.println();
 		
-		// 4
-		for (int i = 0; i < args.length; i++) {
-			
+		// 3 b
+		for (int i = 0; i < 8; i++) {
+			for (int j = 15 - i*2; j > 0; j--) {
+				if (j%2 == 1) {
+					System.out.print("#");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
 		}
+		System.out.println();
+		
+		// 3 c
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < i*2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 15 - i*2; j > 0; j--) {
+				if (j%2 == 1) {
+					System.out.print("#");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		// 3 d
+		for (int i = 0; i < 8; i++) {
+			for (int j = 13; j > i*2 - 1; j--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i*2 + 1; j++) {
+				if (j%2 == 0) {
+					System.out.print("#");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
 	}
 }
