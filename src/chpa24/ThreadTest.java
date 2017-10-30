621c0b861706a5a19de1ff9de0b8ae4e4073ab55
jongtix <jong1145@naver.com>
2017-10-30 17:54:01 +0900

    171030

---
+++ src/chpa24/ThreadTest.java
@@ -0,0 +1,14 @@
+package chpa24;
+
+public class ThreadTest extends MyThread {
+	public static void main(String[] args) {
+		int sum = 1;
+		for (int i = 1; i <= 10; i++) {
+			sum *= i;
+			System.out.println(i + "곱하기 결과: " + sum);
+		}
+		
+		System.out.println();
+		System.out.println("1 ~ 10 곱하기: " + sum);
+	}
+}
